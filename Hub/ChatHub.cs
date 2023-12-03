0a76848342f7e3b46e999490c24c34a908cc5afe
ahamedsuhails <suhias020official@gmail.com>
2023-12-03 13:15:29 +0530

    Add project files.

---
+++ Hub/ChatHub.cs
@@ -0,0 +1,39 @@
+﻿using GroupChatApplication.Models;
+using Microsoft.AspNetCore.SignalR;
+
+namespace GroupChatApplication.Hub
+{
+    public class ChatHub : Microsoft.AspNetCore.SignalR.Hub
+    {
+
+        //dictionary for storing userRoomConnection details
+        private readonly IDictionary<string, UserRoomConnection> _connection;
+
+        public ChatHub(IDictionary<string, UserRoomConnection> connection)
+        {
+            _connection = connection;
+        }
+
+        //method for joining a room
+        public async Task JoinRoom(UserRoomConnection userRoomConnection)
+        {
+            //save connection to dictionary
+            _connection[Context.ConnectionId] = userRoomConnection;
+            // use signalR Hub methods to join a group
+            await Groups.AddToGroupAsync(Context.ConnectionId, groupName: userRoomConnection.RoomName);
+            // notify the group
+            await Clients.Group(userRoomConnection.RoomName)
+                .SendAsync(method: "ReceiveMessage", "Lets Program Bot", $"{userRoomConnection.UserName} has joined the group");
+        }
+
+        //method for sending a message in the group
+        public async Task SendMessage(string message)
+        {
+            if (_connection.TryGetValue(Context.ConnectionId, out UserRoomConnection? userRoomConnection))
+            {
+                await Clients.Group(userRoomConnection.RoomName)
+                    .SendAsync(method: "ReceiveMessage", userRoomConnection.UserName, message, DateTime.Now);
+            }
+        }
+    }
+}
---
+++ Models/UserRoomConnection.cs
@@ -0,0 +1,9 @@
+﻿namespace GroupChatApplication.Models
+{
+    public class UserRoomConnection
+    {
+        public string UserName { get; set; } = string.Empty;
+
+        public string RoomName { get; set; } = string.Empty;
+    }
+}
